9eea2a1ed2a4c60135402d0d56fbf257daa2b514
J <77813539+jae-vh@users.noreply.github.com>
2026-07-07 11:12:27 +0930

    Resize and maintain aspect ratio (hopefully...)
    This is a work in progress and needs to be tested

--- aura-video-analyser.py
+++ aura-video-analyser.py
@@ -1,6 +1,7 @@
 import sys
 
 import cv2
+import numpy as np
 import matplotlib as mpl
 import matplotlib.colors as mplc
 import matplotlib.cm as mtpltcm
@@ -12,10 +13,10 @@ class VideoAnalyser:
     def __init__(self, camera_index):
         print("Initialising...")
 
-        self.cap = cv2.VideoCapture(camera_index)
+        self.cap = cv2.VideoCapture(camera_index, cv2.CAP_ANY)
 
         if not self.cap.isOpened():
-            raise RuntimeError("\u001b[1mInvalid camera device, try a different index.\033[0m")
+            raise RuntimeError("\u001b[1mInvalid camera device, try a different number.\033[0m")
 
         # Dana Barret Aura-Rendering
         COLORMAP = mplc.ListedColormap([ 
@@ -27,12 +28,36 @@ class VideoAnalyser:
 
         self.segmentor = SelfiSegmentation()
 
+        # TODO Get it so we have it fullscreen but maintain aspect ratio
         cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        # self.x, self.y, self.windowWidth, self.windowHeight = cv2.getWindowImageRect("window")
+
+        
 
 
     def render_loop(self):
         """Continuously capture, process, and display aura-rendered video frames."""
+
+        self.x, self.y, self.window_width, self.window_height = cv2.getWindowImageRect("window")
+
+        ret, frame = self.cap.read()
+
+        height, width = frame.shape[:2]
+
+        # This is so we can scale the image to screen size while maintaining aspect ratio
+        scale = min(self.window_width / width, self.window_height / height)
+
+        new_image_width = int(width * scale)
+        new_image_height = int(height * scale)
+
+        x = int((self.window_width - new_image_width) // 2)
+        y = int((self.window_height - new_image_height) // 2)
+
+        translationMatrix = np.float32([[1, 0, x], [0, 1, y]])
+
+
 
         print("Analysing spectral frequencies...")
 
@@ -55,19 +80,22 @@ class VideoAnalyser:
 
             small_image = cv2.resize(colors, (120, 120), interpolation=cv2.INTER_LINEAR)
 
-            # Initialize output image
-            scaled_image = cv2.resize(small_image, (width, height), interpolation=cv2.INTER_NEAREST)
+            # Scale image to screen size
+            # scaled_image = cv2.resize(small_image, (width, height), interpolation=cv2.INTER_NEAREST)
+            scaled_image = cv2.resize(small_image, (new_image_width, new_image_height), interpolation=cv2.INTER_AREA)
 
-            blur = cv2.GaussianBlur(scaled_image, (15,5), 0)
+            translated_image = cv2.warpAffine(scaled_image, translationMatrix, (self.window_width, self.window_height))
+
 
             # Display the resulting frame
-            cv2.imshow("window", blur)
+            cv2.imshow("window", translated_image)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 return
 
     def close(self):
         """Releases camera and closes OpenCV window."""
+        print("Shutdown sequence initiated...")
 
         # When everything done, release the capture
         self.cap.release()
@@ -100,16 +128,17 @@ else:
 
 
 if __name__ == "__main__":
-    LICENSE = """Aura Video Analyser is a registered trademark of Ghostbusters
+    LICENSE = """Aura Video-Analyser is a registered trademark of Ghostbusters
 (c) Copyright 1984 Ghostbusters
 All rights reserved
 """
     
     print("\033[92m")
     print(LICENSE)
+    print("Camera devices: ")
     get_available_cameras()
     
-    camera_index = int(input("Enter camera index: "))
+    camera_index = int(input("Enter camera number: "))
     analyser = VideoAnalyser(camera_index)
 
     try:
